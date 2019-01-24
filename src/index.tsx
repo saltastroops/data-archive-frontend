69e03b22806f53d4c3a00c0bb6ecd305e709e4e1
Eb-Zeero <macebelene@gmail.com>
2019-01-24 14:29:38 +0200

    graphQL end point added

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,27 @@
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloClient } from "apollo-client";
 import { HttpLink } from "apollo-link-http";
-import 'bulma/css/bulma.css'
+import "bulma/css/bulma.css";
 import * as React from "react";
 import { ApolloProvider } from "react-apollo";
 import * as ReactDOM from "react-dom";
+import { gqlEndpoint } from "../config";
 import App from "./App";
-import { gqlEndpoint } from "./config";
 
 import * as serviceWorker from "./registerServiceWorker";
 
 const client = new ApolloClient({
-    cache: new InMemoryCache(),
-    link: new HttpLink({
-        uri: gqlEndpoint
-    })
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: gqlEndpoint
+  })
 });
 ReactDOM.render(
-    <ApolloProvider client={client}>
-        <div className={'container'}>
-            <App />
-        </div>
-    </ApolloProvider>,
-    document.getElementById("root")
+  <ApolloProvider client={client}>
+    <div className={"container"}>
+      <App />
+    </div>
+  </ApolloProvider>,
+  document.getElementById("root")
 );
 serviceWorker.unregister();
