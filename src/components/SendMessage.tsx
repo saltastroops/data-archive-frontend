fbcddc8383f0a39eb79110e9ee0369d8f2401a48
Christian Hettlage <hettlage@saao.ac.za>
2019-01-30 16:43:58 +0200

    removed linting rules

--- src/components/SendMessage.tsx
+++ src/components/SendMessage.tsx
@@ -1,32 +1,32 @@
-import gql from "graphql-tag"
+import gql from "graphql-tag";
 import * as React from "react";
 import { ChangeEvent, Component } from "react";
 import { Mutation } from "react-apollo";
 
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const PaddedDiv = styled.div`
   padding: 15px;
 `;
 
 interface ISubmitButtonProps {
-    loading: boolean
+  loading: boolean;
 }
 const SubmitButton = styled.button.attrs({
-    className: "button primary"
+  className: "button primary"
 })`
   && {
     background-color: ${(props: ISubmitButtonProps) =>
-    props.loading ? "lightblue" : "blue"};
+      props.loading ? "lightblue" : "blue"};
     color: white;
   }
 `;
 const MessageTextArea = styled.textarea.attrs({
-    className: 'textarea'
+  className: "textarea"
 })`
   && {
-  min-width: 50%;
-  width: 50%;
+    min-width: 50%;
+    width: 50%;
   }
 `;
 
@@ -37,61 +37,60 @@ const POST_MESSAGE_MUTATION = gql`
 `;
 
 interface ISendMessageState {
-    message: string;
+  message: string;
 }
 class SendMessage extends Component<{}, ISendMessageState> {
-    state = { message: "" };
-    handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-        e.preventDefault();
-        e.persist();
-        this.setState(
-            () =>
-                ({
-                    [e.target.name]: e.target.value
-                } as any)
-        );
+  public state = { message: "" };
+
+  public render() {
+    const onSubmit = async (e: any) => {
+      e.preventDefault();
+      this.setState(() => ({
+        message: ""
+      }));
     };
-    render() {
-        return (
-            <Mutation
-                mutation={POST_MESSAGE_MUTATION}
-                variables={{ text: this.state.message }}
-            >
-                {(sendMessage, { loading, error }) => (
-                    <>
-                        {error && <div>{error.message}</div>}
-                        <form
-                            onSubmit={async e => {
-                                e.preventDefault();
-                                await sendMessage();
-                                this.setState(() => ({
-                                    message: ""
-                                }));
-                            }}
-                        >
-                            <fieldset disabled={loading} aria-disabled=
-                                {loading}>
-                                <PaddedDiv>
-                                    <MessageTextArea
-                                        name="message"
-                                        placeholder="Type your message"
-                                        onChange={this.handleChange}
-                                        value={this.state.message}
-                                    />
-                                </PaddedDiv>
-                                <PaddedDiv>
-                                    <SubmitButton loading={loading}
-                                                  type="submit">
-                                        {loading ? "Submitting..." : "Submit"}
-                                    </SubmitButton>
-                                </PaddedDiv>
-                            </fieldset>
-                        </form>
-                    </>
-                )}
-            </Mutation>
-        );
-    }
+
+    return (
+      <Mutation
+        mutation={POST_MESSAGE_MUTATION}
+        variables={{ text: this.state.message }}
+      >
+        {(sendMessage, { loading, error }) => (
+          <>
+            {error && <div>{error.message}</div>}
+            <form onSubmit={onSubmit}>
+              <fieldset disabled={loading} aria-disabled={loading}>
+                <PaddedDiv>
+                  <MessageTextArea
+                    name="message"
+                    placeholder="Type your message"
+                    onChange={this.handleChange}
+                    value={this.state.message}
+                  />
+                </PaddedDiv>
+                <PaddedDiv>
+                  <SubmitButton loading={loading} type="submit">
+                    {loading ? "Submitting..." : "Submit"}
+                  </SubmitButton>
+                </PaddedDiv>
+              </fieldset>
+            </form>
+          </>
+        )}
+      </Mutation>
+    );
+  }
+
+  private handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    e.preventDefault();
+    e.persist();
+    this.setState(
+      () =>
+        ({
+          [e.target.name]: e.target.value
+        } as any)
+    );
+  };
 }
 
-export default SendMessage
+export default SendMessage;
