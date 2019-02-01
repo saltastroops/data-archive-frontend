103139fda8556a82cb078cdd1746e872216d0a7a
Christian Hettlage <hettlage@saao.ac.za>
2019-02-01 12:10:22 +0200

    added failing tests

---
+++ src/__tests__/NavigationBar.test.tsx
@@ -0,0 +1,17 @@
+describe("NavigationBar", () => {
+  it("shows the correct content when the user is not logged in", () => {
+    expect(true).toBe(false);
+  });
+
+  it("shows the correct content when the user is logged in", () => {
+    expect(true).toBe(false);
+  });
+
+  it("shows the correct content when the user is an administrator", () => {
+    expect(true).toBe(false);
+  });
+
+  it("calls the logout function when the logout link is clicked", () => {
+    expect(true).toBe(false);
+  });
+});
