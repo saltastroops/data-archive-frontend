5ba769bd3623f6ff37162998154f32b348e0852d
Eb-Zeero <macebelene@gmail.com>
2019-03-07 13:55:45 +0200

    user in state and logout method

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,32 @@ import * as React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import NavigationBar from "./components/NavigationBar";
 
-class App extends React.Component {
+class App extends React.Component<any, any> {
+  state = {
+    user: {
+      isAdmin: () => true,
+      name: "Nhlavu",
+      username: "nhlavu"
+    }
+  };
+
+  logout = () => {
+    this.setState(() => ({
+      ...this.state,
+      user: undefined
+    }));
+  };
   public render() {
     /* TODO:
      * user is currently unknown so I am using a dummy user
      * this will affect the test of this component after user is defined
      * */
-    const user = {
-      isAdmin: () => true,
-      name: "Nhlavu",
-      username: "Nhlavu"
-    };
-    // const user = undefined;
+    const { user } = this.state;
+
     return (
       <Router>
         <>
-          <NavigationBar
-            user={user}
-            logout={() => {
-              return;
-            }}
-          />
+          <NavigationBar user={user} logout={this.logout} />
         </>
       </Router>
     );
