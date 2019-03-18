100e0daed63f5d5b83eee5832c4817097f833596
Christian Hettlage <hettlage@saao.ac.za>
2019-03-18 21:09:14 +0200

    changed routing

--- src/components/AppRoute.tsx
+++ src/components/AppRoute.tsx
@@ -1,16 +1,9 @@
 import * as React from "react";
-import { Route } from "react-router";
+import { Redirect, Route } from "react-router-dom";
 import Message from "./basicComponents/Message";
 import { canViewPage, isKnownPath } from "./navUtil";
 
-interface IUser {
-  user?: {
-    name: string;
-    username: string;
-    isAdmin: () => boolean;
-  };
-}
-class AppRoute extends React.Component<IUser, any> {
+class AppRoute extends React.Component<any, any> {
   public render() {
     const { user } = this.props;
     const path = window.location.pathname;
@@ -61,47 +54,7 @@ class AppRoute extends React.Component<IUser, any> {
         </>
       );
     }
-    return (
-      <>
-        <Route
-          exact={true}
-          path="/"
-          render={() => <h1 className={"title"}>Main Page</h1>}
-        />
-        <Route
-          exact={true}
-          path="/register"
-          component={() => <h1 className={"title"}>User register</h1>}
-        />
-
-        <Route
-          exact={true}
-          path="/login"
-          component={() => <h1 className={"title"}>Login page</h1>}
-        />
-        <Route
-          exact={true}
-          path="/account"
-          component={() => <h1 className={"title"}>User account</h1>}
-        />
-        <Route
-          exact={true}
-          path="/data-requests"
-          component={() => <h1 className={"title"}>Data request page</h1>}
-        />
-        <Route
-          exact={true}
-          path="/cart"
-          component={() => <h1 className={"title"}>Cart page</h1>}
-        />
-
-        <Route
-          exact={true}
-          path="/admin"
-          component={() => <h1 className={"title"}>Admin page</h1>}
-        />
-      </>
-    );
+    return <></>;
   }
 }
 
