91ee1ea6d54590af0a41d56c8f9b973985baa3cf
Christian Hettlage <hettlage@saao.ac.za>
2019-03-19 22:10:51 +0200

    removed obsolete test

--- src/__tests__/api.test.ts
+++ src/__tests__/api.test.ts
@@ -39,38 +39,4 @@ describe("Api", () => {
     // Expect the successful logged in response message
     expect(response.data.message).toContain("logged in");
   });
-
-  it("does not logs in the user into the system", async () => {
-    // Mocking the axios post request
-    (mockAxios.post as any).mockImplementationOnce(
-      async () =>
-        await {
-          data: {
-            message: "The username or password is wrong.",
-            success: false
-          },
-          status: 401
-        }
-    );
-
-    const response = await api.auth.login({
-      password: "wrong",
-      username: "wrong"
-    });
-
-    // Expect the axios post request to have been called once.
-    expect(mockAxios.post).toHaveBeenCalledTimes(1);
-
-    // Expect the axios post to have been called with the correct arguments.
-    expect(mockAxios.post).toHaveBeenCalledWith("/auth/login", {
-      password: "wrong",
-      username: "wrong"
-    });
-
-    // Expect the response success to be false.
-    expect(response.data.success).toBe(false);
-
-    // Expect the unsuccessful logged in response message.
-    expect(response.data.message).toContain("wrong");
-  });
 });
