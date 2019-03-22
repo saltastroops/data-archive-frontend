8faa4bde54247c40d566f0d3ad3e668382c58b8b
Christian Hettlage <hettlage@saao.ac.za>
2019-03-22 15:55:54 +0200

    unit test for navigating

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 import wait from "waait";
 import App from "../App";
 import { MemoryRouter } from "react-router-dom";
-import { Route } from "react-router-dom";
+import click from "../util/click";
 
 describe("App", () => {
   it("should be defined", () => {
@@ -24,4 +24,33 @@ describe("App", () => {
       )
     ).toBeDefined();
   });
+
+  it("should go to the search page if the home link is clicked", async () => {
+    // Go to another page
+    const wrapper = mount(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+    const cartLink = wrapper.find('a[href="/cart"]');
+    click(cartLink.first());
+
+    await wait(0);
+    wrapper.update();
+
+    // No search form is shown any longer.
+    expect(wrapper.find("SearchForm").length).toBe(0);
+
+    // Go back to the "home page" again.
+    const homeLink = wrapper.find('a[href="/"]');
+    click(homeLink.first());
+
+    await wait(0);
+    wrapper.update();
+
+    // The search form is shown now.
+    expect(wrapper.find("SearchForm").length).not.toBe(0);
+  });
+
+  // TODO: Add tests for links to other pages.
 });
