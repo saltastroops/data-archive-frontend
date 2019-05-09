edd118a5e750fb9767ced959a4051683b1c271af
Sifiso Myeza <myezasifiso11@gmail.com>
2019-05-09 19:07:59 +0200

    Fixed data request file download test

--- src/__tests__/components/dataRequest/DataRequestsForm.test.tsx
+++ src/__tests__/components/dataRequest/DataRequestsForm.test.tsx
@@ -1,11 +1,16 @@
 import { mount } from "enzyme";
 import toJson from "enzyme-to-json";
 import * as React from "react";
+import { MockedProvider } from "react-apollo/test-utils";
 import DataRequestsForm from "../../../components/dataRequest/DataRequestsForm";
 
 describe("DataRequestsForm", () => {
   it("renders the DataRequestForm component correctly", async () => {
-    const wrapper = mount(<DataRequestsForm />);
+    const wrapper = mount(
+      <MockedProvider>
+        <DataRequestsForm />
+      </MockedProvider>
+    );
 
     expect(toJson(wrapper)).toMatchSnapshot();
   });
