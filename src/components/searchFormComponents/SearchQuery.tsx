b0bf560d8c0bb87e3a80fbeb541755e3f32c5c2d
Christian Hettlage <hettlage@saao.ac.za>
2019-07-20 18:40:12 +0200

    added preload method

--- src/components/searchFormComponents/SearchQuery.tsx
+++ src/components/searchFormComponents/SearchQuery.tsx
@@ -5,6 +5,7 @@ interface ISearchQueryProps {
   children: (v: ISearchQueryRenderArg) => React.ReactNode;
   client: ApolloClient<any>;
   options: QueryOptions;
+  skip?: boolean;
 }
 
 interface ISearchQueryState {
@@ -33,7 +34,7 @@ interface ISearchQueryRenderArg {
  *     An error raised while executing a query.
  * fetch:
  *     A function which taken an options object and then performs a query. The
- *     options are the same as those used for the query function of an Apollo
+ *     options are the same as those used for the query method of an Apollo
  *     client (https://www.apollographql.com/docs/react/api/apollo-client/).
  *     The data, error and loading properties are updated by the query.
  * loading:
@@ -42,15 +43,32 @@ interface ISearchQueryRenderArg {
  *     Preload data, but do not update the data, error or loading values. The
  *     options are the same as for the fetch method.
  *
+ * The component takes the following properties:
  *
+ * client:
+ *     The Apollo client to use.
+ * options:
+ *     The query options. These are the same as for the query method of an
+ *     Apollo client
+ *     (https://www.apollographql.com/docs/react/api/apollo-client/).
+ * skip:
+ *     Whether to skip, i.e. not to execute a query.
  */
-class SearchQuery extends React.Component<ISearchQueryProps, any> {
+class SearchQuery extends React.Component<
+  ISearchQueryProps,
+  ISearchQueryState
+> {
+  state = {
+    data: undefined,
+    error: undefined,
+    loading: false
+  };
   constructor(props: ISearchQueryProps) {
     super(props);
   }
 
   componentDidMount(): void {
-    this.fetch(this.props.options);
+    this.fetch(this.props.options, !!this.props.skip);
   }
 
   render() {
@@ -59,8 +77,21 @@ class SearchQuery extends React.Component<ISearchQueryProps, any> {
     );
   }
 
-  fetch = (options: QueryOptions) => {
-    if (options.fetchResults === false) {
+  /**
+   * Fetch data.
+   *
+   * The options passed to this method are the same as those for the query
+   * method of the Apollo client.
+   *
+   * Parameters
+   * ----------
+   * options: QueryOptions
+   *     Query options.
+   * skip: boolean
+   *     Whether the query should be skipped, i.e. not be executed.
+   */
+  fetch = (options: QueryOptions, skip: boolean) => {
+    if (skip) {
       return;
     }
     this.setState(() => ({ data: undefined, error: undefined, loading: true }));
@@ -76,6 +107,18 @@ class SearchQuery extends React.Component<ISearchQueryProps, any> {
       .catch(error =>
         this.setState(() => ({ data: undefined, error, loading: false }))
       );
+  };
+
+  /**
+   * Preload data.
+   *
+   * This methods loads the data (so that the cache is updated) but does not
+   * update the state. This is mainly intended for preloading data for the
+   * next page if pagination is used.
+   */
+  preload = (options: QueryOptions) => {
+    // Load data, but do not update the state
+    this.props.client.query(options);
   };
 }
 
