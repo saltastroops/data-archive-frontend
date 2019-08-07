90f0330e6630a93ad12c1f70b052f94f8579e6e9
Eb-Zeero <macebelene@gmail.com>
2019-08-07 17:56:09 +0200

    Cart button change

--- src/components/cart/CartButton.tsx
+++ src/components/cart/CartButton.tsx
@@ -9,11 +9,37 @@ interface ICartButtonProps {
 }
 
 const ButtonContainer = styled.div.attrs({
-  className: "navbar-item is-large"
+  className: "navbar-item is-large is-link"
 })`
   && {
     padding: 10px;
   }
+  && :hover, :focus {
+    
+    background-color: none;
+    text-decoration: none;
+    background-position: 0 -15px;
+    -webkit-transition: background-position .1s linear;
+    -moz-transition: background-position .1s linear;
+    -o-transition: background-position .1s linear;
+    transition: background-position .1s linear;
+`;
+const ButtonSpan = styled.span.attrs({ className: "is-span" })`
+  && {
+    margin-left: 10px;
+  }
+`;
+const Button = styled.span.attrs({
+  className: "button title is-outlined"
+})`
+  && {
+  color: hsl(217, 71%, 53%);
+    padding: 0;
+border: none;
+background: none;
+  }
+  
+}
 `;
 const CartButton = (props: ICartButtonProps) => {
   const { openCart, cartItemsNumber } = props;
@@ -21,18 +47,15 @@ const CartButton = (props: ICartButtonProps) => {
 
   return (
     <ButtonContainer>
-      <button
-        className="button is-link subtitle"
-        onClick={() => openCart(true)}
-      >
+      <Button onClick={() => openCart(true)}>
         <span
           className={"has-badge-rounded has-badge-danger is-span"}
           data-badge={`${badgeNumber}`}
         >
           <FontAwesomeIcon icon={faShoppingCart} />
         </span>
-        CART
-      </button>
+        <ButtonSpan>Cart</ButtonSpan>
+      </Button>
     </ButtonContainer>
   );
 };
