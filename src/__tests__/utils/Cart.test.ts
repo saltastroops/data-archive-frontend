9000bcac80a5dc8900897043f7494fa18ae7dd1f
Christian Hettlage <hettlage@saao.ac.za>
2019-12-02 21:03:13 +0200

    updated unit tests

--- src/__tests__/utils/Cart.test.ts
+++ src/__tests__/utils/Cart.test.ts
@@ -3,32 +3,38 @@ import { Cart, ICartFile } from "../../util/Cart";
 
 describe("Cart", () => {
   it("should have the correct size", () => {
-    let cart = new Cart([
-      {
-        id: "A",
-        name: "File A",
-        observation: { id: "Obs-A", name: "Observation A" }
-      } as ICartFile,
-      { id: "F", name: "File F" } as ICartFile
-    ]);
+    let cart = new Cart(
+      [
+        {
+          id: "A",
+          name: "File A",
+          observation: { id: "Obs-A", name: "Observation A" }
+        } as ICartFile,
+        { id: "F", name: "File F" } as ICartFile
+      ],
+      true
+    );
     expect(cart.size).toBe(2);
 
-    cart = new Cart([
-      { id: "A", name: "File A" } as ICartFile,
-      { id: "B", name: "File B" } as ICartFile,
-      {
-        id: "C",
-        name: "File C",
-        observation: { id: "Obs-B", name: "Observation B" }
-      } as ICartFile,
-      { id: "D", name: "File D" } as ICartFile,
-      {
-        id: "E",
-        name: "File E",
-        observation: { id: "Obs-A", name: "Observation A" }
-      } as ICartFile,
-      { id: "F", name: "File F" } as ICartFile
-    ]);
+    cart = new Cart(
+      [
+        { id: "A", name: "File A" } as ICartFile,
+        { id: "B", name: "File B" } as ICartFile,
+        {
+          id: "C",
+          name: "File C",
+          observation: { id: "Obs-B", name: "Observation B" }
+        } as ICartFile,
+        { id: "D", name: "File D" } as ICartFile,
+        {
+          id: "E",
+          name: "File E",
+          observation: { id: "Obs-A", name: "Observation A" }
+        } as ICartFile,
+        { id: "F", name: "File F" } as ICartFile
+      ],
+      true
+    );
     expect(cart.size).toBe(6);
   });
 
@@ -37,14 +43,17 @@ describe("Cart", () => {
       { id: "A", name: "File A" } as ICartFile,
       { id: "F", name: "File F" } as ICartFile
     ];
-    expect(new Cart(files).files).toBe(files);
+    expect(new Cart(files, true).files).toBe(files);
   });
 
   it("should know what it contains", () => {
-    const cart = new Cart([
-      { id: "A", name: "File A" } as ICartFile,
-      { id: "F", name: "File F" } as ICartFile
-    ]);
+    const cart = new Cart(
+      [
+        { id: "A", name: "File A" } as ICartFile,
+        { id: "F", name: "File F" } as ICartFile
+      ],
+      true
+    );
 
     expect(cart.contains({ id: "A" } as ICartFile)).toBe(true);
     expect(cart.contains({ id: "F" } as ICartFile)).toBe(true);
@@ -54,14 +63,17 @@ describe("Cart", () => {
   });
 
   it("should add files", () => {
-    const cart = new Cart([
-      { id: "A", name: "File A" } as ICartFile,
-      { id: "B", name: "File B" } as ICartFile,
-      { id: "C", name: "File C" } as ICartFile,
-      { id: "D", name: "File D" } as ICartFile,
-      { id: "E", name: "File E" } as ICartFile,
-      { id: "F", name: "File F" } as ICartFile
-    ]);
+    const cart = new Cart(
+      [
+        { id: "A", name: "File A" } as ICartFile,
+        { id: "B", name: "File B" } as ICartFile,
+        { id: "C", name: "File C" } as ICartFile,
+        { id: "D", name: "File D" } as ICartFile,
+        { id: "E", name: "File E" } as ICartFile,
+        { id: "F", name: "File F" } as ICartFile
+      ],
+      true
+    );
     cart.add([
       { id: "A", name: "File A" } as ICartFile,
       { id: "Q", name: "File Q" } as ICartFile,
@@ -80,14 +92,17 @@ describe("Cart", () => {
   });
 
   it("should remove files", () => {
-    const cart = new Cart([
-      { id: "A", name: "File A" } as ICartFile,
-      { id: "B", name: "File B" } as ICartFile,
-      { id: "C", name: "File C" } as ICartFile,
-      { id: "D", name: "File D" } as ICartFile,
-      { id: "E", name: "File E" } as ICartFile,
-      { id: "F", name: "File F" } as ICartFile
-    ]);
+    const cart = new Cart(
+      [
+        { id: "A", name: "File A" } as ICartFile,
+        { id: "B", name: "File B" } as ICartFile,
+        { id: "C", name: "File C" } as ICartFile,
+        { id: "D", name: "File D" } as ICartFile,
+        { id: "E", name: "File E" } as ICartFile,
+        { id: "F", name: "File F" } as ICartFile
+      ],
+      true
+    );
     cart.remove([
       { id: "A", name: "File A" } as ICartFile,
       { id: "Q", name: "File Q" } as ICartFile,
@@ -102,65 +117,71 @@ describe("Cart", () => {
   });
 
   it("should clear the cart", () => {
-    const cart = new Cart([
-      {
-        id: "A",
-        name: "File A",
-        observation: { id: "Obs-A", name: "Observation A" }
-      } as ICartFile,
-      {
-        id: "D",
-        name: "File D",
-        observation: { id: "Obs-B", name: "Observation B" }
-      } as ICartFile,
-      {
-        id: "C",
-        name: "File C",
-        observation: { id: "Obs-D", name: " Observation D" }
-      } as ICartFile
-    ]);
+    const cart = new Cart(
+      [
+        {
+          id: "A",
+          name: "File A",
+          observation: { id: "Obs-A", name: "Observation A" }
+        } as ICartFile,
+        {
+          id: "D",
+          name: "File D",
+          observation: { id: "Obs-B", name: "Observation B" }
+        } as ICartFile,
+        {
+          id: "C",
+          name: "File C",
+          observation: { id: "Obs-D", name: " Observation D" }
+        } as ICartFile
+      ],
+      true
+    );
     cart.clear();
     expect(cart.groupByObservation()).toEqual(new Map());
   });
 
   it("should provide cart items grouped by observation", () => {
-    const cart = new Cart([
-      {
-        id: "A",
-        name: "File A",
-        observation: { id: "Obs-A", name: "Observation A" }
-      } as ICartFile,
-      {
-        id: "D",
-        name: "File D",
-        observation: { id: "Obs-B", name: "Observation B" }
-      } as ICartFile,
-      {
-        id: "C",
-        name: "File C",
-        observation: { id: "Obs-D", name: " Observation D" }
-      } as ICartFile,
-      {
-        id: "E",
-        name: "File E",
-        observation: { id: "Obs-A", name: "Observation A" }
-      } as ICartFile,
-      {
-        id: "F",
-        name: "File F",
-        observation: { id: "Obs-A", name: "Observation A" }
-      } as ICartFile,
-      {
-        id: "G",
-        name: "File G",
-        observation: { id: "Obs-C", name: "Observation C" }
-      } as ICartFile,
-      {
-        id: "H",
-        name: "File H",
-        observation: { id: "Obs-B", name: "Observation B" }
-      } as ICartFile
-    ]);
+    const cart = new Cart(
+      [
+        {
+          id: "A",
+          name: "File A",
+          observation: { id: "Obs-A", name: "Observation A" }
+        } as ICartFile,
+        {
+          id: "D",
+          name: "File D",
+          observation: { id: "Obs-B", name: "Observation B" }
+        } as ICartFile,
+        {
+          id: "C",
+          name: "File C",
+          observation: { id: "Obs-D", name: " Observation D" }
+        } as ICartFile,
+        {
+          id: "E",
+          name: "File E",
+          observation: { id: "Obs-A", name: "Observation A" }
+        } as ICartFile,
+        {
+          id: "F",
+          name: "File F",
+          observation: { id: "Obs-A", name: "Observation A" }
+        } as ICartFile,
+        {
+          id: "G",
+          name: "File G",
+          observation: { id: "Obs-C", name: "Observation C" }
+        } as ICartFile,
+        {
+          id: "H",
+          name: "File H",
+          observation: { id: "Obs-B", name: "Observation B" }
+        } as ICartFile
+      ],
+      true
+    );
     const expected = new Map<string, ICartFile[]>();
     expected.set("Obs-A", [
       {
