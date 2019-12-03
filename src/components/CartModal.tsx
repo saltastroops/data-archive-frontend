70d1240b6e9130eed30d4e514ef90e9ff53704ab
Christian Hettlage <hettlage@saao.ac.za>
2019-12-03 22:38:23 +0200

    added checkbox for including calibrations

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -18,6 +18,7 @@ import {
   CART_QUERY,
   CLEAR_CART_MUTATION,
   ICartFile,
+  INCLUDE_CALIBRATIONS_IN_CART_MUTATION,
   REMOVE_FROM_CART_MUTATION
 } from "../util/Cart";
 import CartFileRow from "./cart/CartFileRow";
@@ -27,6 +28,16 @@ interface ICart {
   openCart: (open: boolean) => void;
   user?: any;
 }
+
+const LargeCheckbox = styled.input.attrs({
+  className: "checkbox",
+  type: "checkbox"
+})`
+  && {
+    width: 18px;
+    height: 18px;
+  }
+`;
 
 const ErrorMessage = styled.p.attrs({
   className: "error tile"
@@ -51,181 +62,216 @@ class CartModal extends React.Component<ICart, any> {
         mutation={REMOVE_FROM_CART_MUTATION}
         refetchQueries={[{ query: CART_QUERY }]}
       >
-        {(removeFromCart: any) => {
-          const cartContent: any = cache.readQuery({ query: CART_QUERY }) || {
-            cart: { files: [], includeCalibrations: true }
-          };
-          const groupedCart: any = [];
-          const cart = new Cart(
-            cartContent.cart.files,
-            cartContent.cart.includeCalibrations
-          );
-          cart.groupByObservation().forEach((v, k) => {
-            groupedCart.push({ files: v, id: k, name: v[0].observation.name });
-          });
-          const dataFileIds = Array.from(cart.files).map(file =>
-            parseInt(file.id, 10)
-          );
-          return (
-            <Mutation
-              mutation={CREATE_DATA_REQUEST}
-              refetchQueries={[
-                {
-                  query: USER_DATA_REQUESTS_QUERY,
-                  variables: {
-                    limit: 5,
-                    startIndex: 0
-                  }
-                }
-              ]}
-            >
-              {(createDataRequest: any, { error }: any) => (
+        {(removeFromCart: any) => (
+          <Mutation
+            mutation={INCLUDE_CALIBRATIONS_IN_CART_MUTATION}
+            refetchQueries={[{ query: CART_QUERY }]}
+          >
+            {(includeCalibrations: any) => {
+              const cartContent: any = cache.readQuery({
+                query: CART_QUERY
+              }) || {
+                cart: { files: [], includeCalibrations: true }
+              };
+              const groupedCart: any = [];
+              const cart = new Cart(
+                cartContent.cart.files,
+                cartContent.cart.includeCalibrations
+              );
+              cart.groupByObservation().forEach((v, k) => {
+                groupedCart.push({
+                  files: v,
+                  id: k,
+                  name: v[0].observation.name
+                });
+              });
+              const dataFileIds = Array.from(cart.files).map(file =>
+                parseInt(file.id, 10)
+              );
+              const includeCalibrationFiles = cart.includeCalibrations;
+
+              const updateIncludeCalibrations = async (
+                event: React.ChangeEvent<HTMLInputElement>
+              ) => {
+                includeCalibrations({
+                  variables: { includeCalibrations: event.target.checked }
+                });
+              };
+
+              return (
                 <Mutation
-                  mutation={CLEAR_CART_MUTATION}
-                  refetchQueries={[{ query: CART_QUERY }]}
+                  mutation={CREATE_DATA_REQUEST}
+                  refetchQueries={[
+                    {
+                      query: USER_DATA_REQUESTS_QUERY,
+                      variables: {
+                        limit: 5,
+                        startIndex: 0
+                      }
+                    }
+                  ]}
                 >
-                  {(clearCart: any) => (
-                    <Modal
-                      open={open}
-                      onClose={() => openCart(false)}
-                      center={true}
+                  {(createDataRequest: any, { error }: any) => (
+                    <Mutation
+                      mutation={CLEAR_CART_MUTATION}
+                      refetchQueries={[{ query: CART_QUERY }]}
                     >
-                      <div className={"section"}>
-                        {error ? (
-                          <ErrorMessage>
-                            {error.message
-                              .replace("Network error: ", "")
-                              .replace("GraphQL error: ", "")}
-                          </ErrorMessage>
-                        ) : null}
-                        <div
-                          className={
-                            "table-container cart-table-scroll-vertically"
-                          }
+                      {(clearCart: any) => (
+                        <Modal
+                          open={open}
+                          onClose={() => openCart(false)}
+                          center={true}
                         >
-                          <table
-                            className={
-                              "table is-fullwidth is-outlined is-bordered"
-                            }
-                          >
-                            <thead>
-                              <tr>
-                                <th>Observation</th>
-                                <th>Remove</th>
-                                <th>Filename</th>
-                                <th>Target name</th>
-                              </tr>
-                            </thead>
-                            <tbody>
-                              {!groupedCart.length ? (
-                                <tr>
-                                  <td colSpan={4} rowSpan={20}>
-                                    <div
-                                      className={
-                                        "column is-center notification"
+                          <div className={"section"}>
+                            {error ? (
+                              <ErrorMessage>
+                                {error.message
+                                  .replace("Network error: ", "")
+                                  .replace("GraphQL error: ", "")}
+                              </ErrorMessage>
+                            ) : null}
+                            <div
+                              className={
+                                "table-container cart-table-scroll-vertically"
+                              }
+                            >
+                              <table
+                                className={
+                                  "table is-fullwidth is-outlined is-bordered"
+                                }
+                              >
+                                <thead>
+                                  <tr>
+                                    <th>Observation</th>
+                                    <th>Remove</th>
+                                    <th>Filename</th>
+                                    <th>Target name</th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  {!groupedCart.length ? (
+                                    <tr>
+                                      <td colSpan={4} rowSpan={20}>
+                                        <div
+                                          className={
+                                            "column is-center notification"
+                                          }
+                                        >
+                                          <p
+                                            className={
+                                              "has-text-centered has-text-info is-size-3"
+                                            }
+                                          >
+                                            The cart is empty.
+                                          </p>
+                                        </div>
+                                      </td>
+                                    </tr>
+                                  ) : (
+                                    groupedCart.map(
+                                      (obz: any, obzIndex: number) => {
+                                        return Array.from(obz.files).map(
+                                          (file: any, index: number) => (
+                                            <CartFileRow
+                                              index={index}
+                                              id={file.id}
+                                              key={file.id}
+                                              file={file}
+                                              obzIndex={obzIndex}
+                                              remove={this.remove}
+                                              removeFromCart={removeFromCart}
+                                            />
+                                          )
+                                        );
                                       }
+                                    )
+                                  )}
+                                </tbody>
+                              </table>
+                              <div>
+                                <label>
+                                  <LargeCheckbox
+                                    checked={cart.includeCalibrations}
+                                    onChange={updateIncludeCalibrations}
+                                  />{" "}
+                                  Include calibration files
+                                </label>
+                              </div>
+                            </div>
+                          </div>
+                          <div className={"section"}>
+                            <div className={"columns cart-buttons-section"}>
+                              <div className={"column"}>
+                                {!user ? (
+                                  <NavLink to={"login"}>
+                                    <button
+                                      className={"button is-primary"}
+                                      onClick={() => openCart(false)}
                                     >
-                                      <p
-                                        className={
-                                          "has-text-centered has-text-info is-size-3"
+                                      <span>
+                                        Login{" "}
+                                        <FontAwesomeIcon icon={faUserPlus} />
+                                      </span>
+                                    </button>
+                                  </NavLink>
+                                ) : (
+                                  <NavLink to={"data-requests"}>
+                                    <button
+                                      className={"button is-primary"}
+                                      onClick={() => {
+                                        this.createDataRequest(
+                                          createDataRequest,
+                                          clearCart,
+                                          dataFileIds,
+                                          includeCalibrationFiles
+                                        );
+                                        if (!error) {
+                                          openCart(false);
                                         }
-                                      >
-                                        The cart is empty.
-                                      </p>
-                                    </div>
-                                  </td>
-                                </tr>
-                              ) : (
-                                groupedCart.map(
-                                  (obz: any, obzIndex: number) => {
-                                    return Array.from(obz.files).map(
-                                      (file: any, index: number) => (
-                                        <CartFileRow
-                                          index={index}
-                                          id={file.id}
-                                          key={file.id}
-                                          file={file}
-                                          obzIndex={obzIndex}
-                                          remove={this.remove}
-                                          removeFromCart={removeFromCart}
-                                        />
-                                      )
-                                    );
-                                  }
-                                )
-                              )}
-                            </tbody>
-                          </table>
-                        </div>
-                      </div>
-                      <div className={"section"}>
-                        <div className={"columns cart-buttons-section"}>
-                          <div className={"column"}>
-                            {!user ? (
-                              <NavLink to={"login"}>
+                                      }}
+                                    >
+                                      <span>
+                                        Request{" "}
+                                        <FontAwesomeIcon icon={faDownload} />
+                                      </span>
+                                    </button>
+                                  </NavLink>
+                                )}
+                              </div>
+                              <div className={"column"}>
                                 <button
-                                  className={"button is-primary"}
+                                  className={"button is-danger"}
                                   onClick={() => openCart(false)}
                                 >
                                   <span>
-                                    Login <FontAwesomeIcon icon={faUserPlus} />
+                                    Close{" "}
+                                    <FontAwesomeIcon icon={faWindowClose} />
                                   </span>
                                 </button>
-                              </NavLink>
-                            ) : (
-                              <NavLink to={"data-requests"}>
+                              </div>
+                              <div className={"column"}>
                                 <button
-                                  className={"button is-primary"}
-                                  onClick={() => {
-                                    this.createDataRequest(
-                                      createDataRequest,
-                                      clearCart,
-                                      dataFileIds
-                                    );
-                                    if (!error) {
-                                      openCart(false);
-                                    }
-                                  }}
+                                  className={"button is-warning"}
+                                  onClick={e =>
+                                    this.remove(e, removeFromCart, cart.files)
+                                  }
                                 >
                                   <span>
-                                    Request{" "}
-                                    <FontAwesomeIcon icon={faDownload} />
+                                    Clear <FontAwesomeIcon icon={faEraser} />
                                   </span>
                                 </button>
-                              </NavLink>
-                            )}
+                              </div>
+                            </div>
                           </div>
-                          <div className={"column"}>
-                            <button
-                              className={"button is-danger"}
-                              onClick={() => openCart(false)}
-                            >
-                              <span>
-                                Close <FontAwesomeIcon icon={faWindowClose} />
-                              </span>
-                            </button>
-                          </div>
-                          <div className={"column"}>
-                            <button
-                              className={"button is-warning"}
-                              onClick={e =>
-                                this.remove(e, removeFromCart, cart.files)
-                              }
-                            >
-                              <span>
-                                Clear <FontAwesomeIcon icon={faEraser} />
-                              </span>
-                            </button>
-                          </div>
-                        </div>
-                      </div>
-                    </Modal>
+                        </Modal>
+                      )}
+                    </Mutation>
                   )}
                 </Mutation>
-              )}
-            </Mutation>
-          );
-        }}
+              );
+            }}
+          </Mutation>
+        )}
       </Mutation>
     );
   }
@@ -245,9 +291,12 @@ class CartModal extends React.Component<ICart, any> {
   createDataRequest = async (
     create: any,
     clearCart: any,
-    dataFilesIds: number[]
+    dataFilesIds: number[],
+    includeCalibrations: boolean
   ) => {
-    await create({ variables: { dataFiles: dataFilesIds } });
+    await create({
+      variables: { dataFiles: dataFilesIds, includeCalibrations }
+    });
     await clearCart();
   };
 }
