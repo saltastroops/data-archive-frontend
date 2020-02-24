88f45acb1f8e797f0ab4488cd9717908fa93ffd7
Lonwabo Zaula <lonwabozaula@gmail.com>
2020-02-24 08:46:19 +0200

    added calibration types to the cart query

--- src/components/searchFormComponents/results/SearchResultsTable.tsx
+++ src/components/searchFormComponents/results/SearchResultsTable.tsx
@@ -353,8 +353,7 @@ class SearchResultsTable extends React.Component<
         {({ data }: any) => {
           const cart = new Cart(
             data.cart.files || [],
-            data.cart.includeStandards,
-            data.cart.includeArcsFlatsBiases,
+            data.cart.includedCalibrationTypes,
             data.cart.includedCalibrationLevels
           );
           return (
