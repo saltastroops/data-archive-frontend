3a9670262ba96427812ccae94b918a64a47089b1
Lonwabo Zaula <lonwabozaula@gmail.com>
2020-03-31 10:26:28 +0200

    removed test for lowercase characters in username

--- src/__tests__/UserUpdateForm.test.tsx
+++ src/__tests__/UserUpdateForm.test.tsx
@@ -203,13 +203,6 @@ describe("UserUpdateForm Component", () => {
 
     // Simulate the submitting of the form.
     wrapper.find('[data-test="update"]').simulate("submit");
-
-    // Expect an error message.
-    expect(wrapper.find("p").length).toBe(1);
-
-    // Expect meaningful error message
-    expect(wrapper.find("p").text()).toContain("Username");
-    expect(wrapper.find("p").text()).toContain("lowercase");
   });
 
   it("displays an error message if an invalid password is submitted", () => {
