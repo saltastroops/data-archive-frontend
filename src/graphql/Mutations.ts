7b90ea6a70f869d21e5e37939fd6b01507e96caa
Christian Hettlage <hettlage@saao.ac.za>
2020-10-19 17:46:03 +0200

    Fixed GraphQL query

--- src/graphql/Mutations.ts
+++ src/graphql/Mutations.ts
@@ -131,7 +131,7 @@ export const CREATE_DATA_REQUEST = gql`
       requestedCalibrationTypes: $includedCalibrationTypes
       requestedCalibrationLevels: $includedCalibrationLevels
     ) {
-      dataRequestId
+      id
     }
   }
 `;
