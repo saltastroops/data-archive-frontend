057794414a59d559e3f05da47dca02659e572686
Christian Hettlage <hettlage@saao.ac.za>
2020-12-14 17:33:26 +0200

    Reformatted code

--- src/components/searchFormComponents/results/SearchResultsTableColumnSelector.tsx
+++ src/components/searchFormComponents/results/SearchResultsTableColumnSelector.tsx
@@ -27,9 +27,7 @@ const SelectorColumns = styled.div`
   justify-content: space-between;
 `;
 
-class SearchResultsTableColumnSelector extends React.Component<
-  ISearchResultsTableColumnSelectorProps
-> {
+class SearchResultsTableColumnSelector extends React.Component<ISearchResultsTableColumnSelectorProps> {
   render() {
     const { closeModal, columns, onChange } = this.props;
 
